a77abab2a1c746df278b2a892a37b6e65bbb17d4
Viktor Persson <viktor.persson@arcsin.se>
2022-05-04 10:30:44 +0200

    Restore module under new name.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# DEPRECATED
-Module has moved to: https://pkg.go.dev/github.com/vikpe/masterstat
+# masterstat [![Go Reference](https://pkg.go.dev/badge/github.com/vikpe/masterstat.svg)](https://pkg.go.dev/github.com/vikpe/masterstat)
+
+> Fetch server addresses from QuakeWorld master servers.
+
+## See also
+* [masterstat-cli](https://github.com/vikpe/masterstat-cli)
---
+++ masterstat.go
@@ -0,0 +1,99 @@
+package masterstat
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/vikpe/udpclient"
+)
+
+func GetServerAddresses(masterAddress string) ([]string, error) {
+	statusPacket := []byte{0x63, 0x0a, 0x00}
+	expectedHeader := []byte{0xff, 0xff, 0xff, 0xff, 0x64, 0x0a}
+	udpClient := udpclient.New()
+	response, err := udpClient.Request(masterAddress, statusPacket, expectedHeader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	responseBody := response[len(expectedHeader):]
+	reader := bytes.NewReader(responseBody)
+	serverAddresses := make([]string, 0)
+
+	for {
+		var rawAddress rawServerAddress
+
+		err = binary.Read(reader, binary.BigEndian, &rawAddress)
+		if err != nil {
+			break
+		}
+
+		serverAddresses = append(serverAddresses, rawAddress.toString())
+	}
+
+	return serverAddresses, nil
+}
+
+func GetServerAddressesFromMany(masterAddresses []string) ([]string, error) {
+	var (
+		wg              sync.WaitGroup
+		mutex           sync.Mutex
+		serverAddresses       = make([]string, 0)
+		masterStatErr   error = nil
+	)
+
+	for _, masterAddress := range masterAddresses {
+		wg.Add(1)
+
+		go func(masterAddress string) {
+			defer wg.Done()
+
+			addresses, err := GetServerAddresses(masterAddress)
+
+			if err != nil {
+				masterStatErr = err
+				return
+			}
+
+			mutex.Lock()
+			serverAddresses = append(serverAddresses, addresses...)
+			mutex.Unlock()
+		}(masterAddress)
+	}
+
+	wg.Wait()
+
+	if masterStatErr != nil {
+		return nil, masterStatErr
+	}
+
+	return uniqueStrings(serverAddresses), nil
+}
+
+type rawServerAddress struct {
+	IpParts [4]byte
+	Port    uint16
+}
+
+func (addr rawServerAddress) toString() string {
+	ip := net.IPv4(addr.IpParts[0], addr.IpParts[1], addr.IpParts[2], addr.IpParts[3]).String()
+	return fmt.Sprintf("%s:%d", ip, addr.Port)
+}
+
+func uniqueStrings(values []string) []string {
+	valueMap := make(map[string]bool, 0)
+	uniqueValues := make([]string, 0)
+
+	for _, value := range values {
+		if !valueMap[value] {
+			uniqueValues = append(uniqueValues, value)
+			valueMap[value] = true
+		}
+	}
+
+	return uniqueValues
+}
